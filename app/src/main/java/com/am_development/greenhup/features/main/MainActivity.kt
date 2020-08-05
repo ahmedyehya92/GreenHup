d82e2dba5a7b8fc7c7d30347447f48b99dcde0ea
Ahmed Yehya <ahmedyehya1992@gmail.com>
2020-08-06 00:51:08 +0200

    sec

--- app/src/main/java/com/am_development/greenhup/features/main/MainActivity.kt
+++ app/src/main/java/com/am_development/greenhup/features/main/MainActivity.kt
@@ -3,11 +3,39 @@ package com.am_development.greenhup.features.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.am_development.greenhup.R
+import com.google.android.material.tabs.TabLayout
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.ic_home))
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.ic_shopping_cart))
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.ic_favorite))
+        tabLayout!!.tabGravity = TabLayout.GRAVITY_FILL
+
+        val adapter = ViewPagerAdapter(
+            this,
+            supportFragmentManager,
+            tabLayout!!.tabCount
+        )
+        viewPager!!.adapter = adapter
+        viewPager.offscreenPageLimit=3
+        viewPager!!.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tabLayout))
+
+        tabLayout!!.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab) {
+                viewPager!!.currentItem = tab.position
+            }
+            override fun onTabUnselected(tab: TabLayout.Tab) {
+
+            }
+            override fun onTabReselected(tab: TabLayout.Tab) {
+
+            }
+        })
     }
 }
---
+++ app/src/main/java/com/am_development/greenhup/features/main/ViewPagerAdapter.kt
@@ -0,0 +1,22 @@
+package com.am_development.greenhup.features.main
+
+import android.content.Context
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.am_development.greenhup.features.home.HomeFragment
+
+class ViewPagerAdapter (private val myContext: Context, fm: FragmentManager, internal var totalTabs: Int) : FragmentPagerAdapter(fm)
+{
+    override fun getItem(position: Int): Fragment {
+        return when(position) {
+            0-> HomeFragment()
+            else -> HomeFragment()
+        }
+    }
+
+    override fun getCount(): Int {
+        return totalTabs
+    }
+
+}
